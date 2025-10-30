9d458a966663b471786f6257eeb3fd392fda5f6d
Raheem-Baig1210 <raheembaig822@gmail.com>
2025-10-31 02:33:31 +0530

    Changes in Backend

--- Backend/controller/schoolCntrl.js
+++ Backend/controller/schoolCntrl.js
@@ -1,5 +1,6 @@
 const schoolMdl = require("../model/school_admin")
 const studentMdl = require("../model/students")
+const teacherMdl = require("../model/teacher")
 const {responseGenerator, hashpassword, comparepassword, generateTokens} = require("../utils/utils")
 
 const loginSchool = async(req,res)=>{
@@ -45,8 +46,78 @@ const studentsAtSchool = async (req,res) => {
     }
 }
 
+const teachersAtSchool = async (req,res) => {
+    try {
+        const {schoolId} = req.params
+        const school = await schoolMdl.findById(schoolId)
+        if(!school){
+            return res.status(400).json({message:"School not found...!!!"})
+        }
+        const teachers = await teacherMdl.find({schoolId}).populate({path: "schoolId", select: "name email location" ,model: "school"})
+        if(teachers.length==0){
+            return res.status(200).json({message: "No teacher is assigned yet...!!!"})
+        }
+        // console.log(teachers)
+        return res.status(200).json({count: teachers.length, teachers})
+    } catch (err) {
+        console.log("Error while fetching the teachers list...!!!",err)
+        return res.status(404).json({message:"Errors in teachersAtSchool...!!!"})
+    }
+}
+
+
+const updateStudent = async(req,res)=>{
+    try {
+        const studentId = req.params.id
+        const updatedData = req.body
+
+        const updatedStudent = await studentMdl.findByIdAndUpdate(studentId,updatedData,{new: true})
+        if(!updatedStudent){
+            return res.status(400).json({message:"Student not found...!!!"})
+        }
+        // console.log("Updated...!!!", updatedStudent)
+        return res.status(200).json({message:"Student Updated Successfully...!!!"})
+    } catch (err) {
+        console.log("Error while updating the student information...!!!",err)
+        return res.status(404).json({message:"Error...!!!"})
+    }
+}
+
+const addNewStudent = async (req,res) => {
+    try {
+        const data = req.body
+        const student = new studentMdl(data)
+
+        const currentYear = new Date().getFullYear()
+        const yearSuffix = currentYear.toString().slice(-2)
+
+        const lastRollStudent = await studentMdl.findOne({rollNumber: {$regex:`^${yearSuffix}`}},{},{sort: {rollNumber: -1}})
+
+        if(lastRollStudent && lastRollStudent.rollNumber){
+            const lastCount = parseInt(lastRollStudent.rollNumber.slice(2));
+            const nextCount = (lastCount+1).toString().padStart(3,"0")
+            student.rollNumber=`${yearSuffix}${nextCount}`
+        }else{
+            student.rollNumber=`${yearSuffix}001`
+        }
+        
+        
+        await student.save()
+        
+        let resp = responseGenerator(true,"Student added Successfully ...!!!");
+        return res.status(200).json(resp)
+    } catch (err) {
+        console.log("Error while adding a new student...!!!",err)
+        return res.status(404).json({message:"Error...!!!"})
+    }
+}
+
+
 
 module.exports = {
     loginSchool,
-    studentsAtSchool
+    studentsAtSchool,
+    addNewStudent,
+    updateStudent,
+    teachersAtSchool
 }
--- Backend/routes/school_routes.js
+++ Backend/routes/school_routes.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const school_cntrl = require("../controller/schoolCntrl")
+const admin_cntrl = require("../controller/admincntrl")
 const middleware  = require("../middleware/isLoggedIn")
 
 router.post("/loginSchool",school_cntrl.loginSchool)
 router.get("/studentsAtSchool/:schoolId", school_cntrl.studentsAtSchool)
+router.put("/updateStudent/:id",middleware.isLoggedIn,school_cntrl.updateStudent)
+router.post("/addNewStudent",middleware.isLoggedIn,school_cntrl.addNewStudent)
+router.put("/isActiveStudent/:id", middleware.isLoggedIn,admin_cntrl.deleteStudent)
+router.get("/teachersAtSchool/:schoolId",school_cntrl.teachersAtSchool)
 
 module.exports=router
